431dd1935f4dae02a85175e79ef55df5f6989707
Hojjat Mehri <hojjatmehri@gmail.com>
2021-04-19 03:07:15 +0430

    First time

---
+++ app/src/main/java/com/hojjatmehri/hometoast/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hojjatmehri.hometoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
