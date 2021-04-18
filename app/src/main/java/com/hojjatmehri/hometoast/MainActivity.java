b3e5eaed127f827dd6197813b4a6b4c133315b92
Hojjat Mehri <hojjatmehri@gmail.com>
2021-04-19 03:38:25 +0430

    toast Upload text bug fixed3

--- app/src/main/java/com/hojjatmehri/hometoast/MainActivity.java
+++ app/src/main/java/com/hojjatmehri/hometoast/MainActivity.java
@@ -2,15 +2,7 @@ package com.hojjatmehri.hometoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
