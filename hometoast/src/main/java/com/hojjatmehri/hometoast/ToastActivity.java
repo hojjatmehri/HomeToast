3f44e99bb39db4d6ed7e170db3fed562763d9315
Hojjat Mehri <hojjatmehri@gmail.com>
2021-04-19 03:33:57 +0430

    toast Upload text bug fixed

--- hometoast/src/main/java/com/hojjatmehri/hometoast/ToastActivity.java
+++ hometoast/src/main/java/com/hojjatmehri/hometoast/ToastActivity.java
@@ -1,17 +1,16 @@
 package com.hojjatmehri.hometoast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ToastActivity extends AppCompatActivity {
 
@@ -23,17 +22,19 @@ public class ToastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+
+    }
+
+    public void show(@NonNull String toastText, @Nullable int toastTextColor) {
+
         inflater = getLayoutInflater();
         layout = inflater.inflate(R.layout.activity_toast,
                 (ViewGroup) findViewById(R.id.toast_layout_root));
 
         text = (TextView) layout.findViewById(R.id.text);
-
-    }
-    public void show(@NonNull String toastText,@Nullable int toastTextColor){
         text.setText(toastText);
-        if(toastTextColor != 0)
-        text.setTextColor(toastTextColor);
+        if (toastTextColor != 0)
+            text.setTextColor(toastTextColor);
 
         Toast toast = new Toast(getApplicationContext());
         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
