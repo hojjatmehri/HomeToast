1c51df06432c41443030ca762dbe5e6ba31e54ba
Hojjat Mehri <hojjatmehri@gmail.com>
2021-04-19 17:17:36 +0430

    Added success error methods3

--- hometoast/src/main/java/com/hojjatmehri/hometoast/HomeToast.java
+++ hometoast/src/main/java/com/hojjatmehri/hometoast/HomeToast.java
@@ -8,6 +8,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -74,22 +75,28 @@ public class HomeToast {
         LayoutInflater inflater;
         View layout;
         TextView text;
+        ImageView img;
         inflater = activity.getLayoutInflater();
         layout = inflater.inflate(R.layout.activity_toast,
                 (ViewGroup) activity.findViewById(R.id.toast_layout_root));
 
         relbg = layout.findViewById(R.id.toast_layout_root);
+        img = layout.findViewById(R.id.img_symbol);
         text = (TextView) layout.findViewById(R.id.text);
-        relbg.setBackgroundDrawable(activity.getDrawable(R.drawable.bg_success));
-        relbg.setElevationShadowColor(activity.getColor(R.color.success));
         switch (iStatus){
             case 1:
                 relbg.setBackgroundDrawable(activity.getDrawable(R.drawable.bg_success));
                 relbg.setElevationShadowColor(activity.getColor(R.color.success));
+                img.setBackground(activity.getDrawable(R.drawable.bg_success_icon));
+                img.setImageDrawable(activity.getDrawable(R.drawable.check));
+                img.setColorFilter(activity.getColor(R.color.white));
                 break;
             case 2:
                 relbg.setBackgroundDrawable(activity.getDrawable(R.drawable.bg_error));
                 relbg.setElevationShadowColor(activity.getColor(R.color.error));
+                img.setBackground(activity.getDrawable(R.drawable.bg_error_icon));
+                img.setImageDrawable(activity.getDrawable(R.drawable.close));
+                img.setColorFilter(activity.getColor(R.color.white));
                 break;
 //            default:
 //                relbg.setBackgroundColor(toastBGColor);
